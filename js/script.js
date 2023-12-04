74a1bba17707d7ff9274f2edd012a99ac45f756f
SebastianBuftea <sebibuftea85@gmail.com>
2023-12-04 15:46:26 +0100

    [UPDATE]create folders

---
+++ js/script.js
@@ -0,0 +1,32 @@
+let team=[
+    {
+        nome:"Wayne Bamett",
+        ruolo:"",
+        foto:""
+    },
+    {
+        nome:"Angela Caroll",
+        ruolo:"",
+        foto:""
+    },
+    {
+        nome:"Walter Gordon",
+        ruolo:"",
+        foto:""
+    },
+    {
+        nome:"Angela Lopex",
+        ruolo:"",
+        foto:""
+    },
+    {
+        nome:"Scott Estrada",
+        ruolo:"",
+        foto:""
+    },
+    {
+        nome:"Barbara Ramos",
+        ruolo:"",
+        foto:""
+    }  
+]
